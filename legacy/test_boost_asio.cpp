24a89d4633f5b98c34a65a7d8a422b8a566c757a
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-06-10 14:45:03 +0300

    AsyncResult, add tests

--- legacy/test_boost_asio.cpp
+++ legacy/test_boost_asio.cpp
@@ -19,7 +19,7 @@ TEST(Boost_ASIO, async_result) {
 
     std::string message = "rEsUlT";
     auto handler = [asyncHandler, message]() mutable {
-        return asyncHandler(boost::system::error_code{}, message);
+        asyncHandler(boost::system::error_code{}, message);
     };
 
     auto future = asyncResult.get();
@@ -29,4 +29,56 @@ TEST(Boost_ASIO, async_result) {
     ioContext.run();
 
     ASSERT_EQ(future.get(), "rEsUlT");
+}
+
+#include <tuple>
+
+struct AsyncPromise
+{
+    using Signature = void (boost::system::error_code, const std::string&);
+    using CompletionToken = std::decay_t<decltype(boost::asio::use_future)>;
+
+    using AsyncResult = boost::asio::async_result<CompletionToken, Signature>;
+    using AsyncHandler = AsyncResult::completion_handler_type;
+
+    static auto create(boost::asio::io_context& ioContext) {
+        AsyncHandler asyncHandler{boost::asio::use_future};
+        AsyncResult asyncResult{asyncHandler};
+
+        auto promise = [&ioContext, asyncHandler](const std::string& message) mutable {
+            auto handler = [asyncHandler, message]() mutable {
+                return asyncHandler(boost::system::error_code{}, message);
+            };
+            ioContext.post(handler);
+        };
+
+        return std::make_tuple(promise, asyncResult.get());
+    }
+};
+
+#include <thread>
+
+TEST(AsyncResult, simple) {
+    boost::asio::io_context ioContext;
+    auto [promise, future] = AsyncPromise::create(ioContext);
+
+    promise("rESULT");
+    ioContext.run();
+
+    ASSERT_EQ(future.get(), "rESULT");
+}
+
+TEST(AsyncResult, thread) {
+    boost::asio::io_context ioContext;
+    auto [promise, future] = AsyncPromise::create(ioContext);
+
+    auto worker = [promise = promise]() mutable {
+        promise("ReSuLt");
+    };
+    std::thread thread{worker};
+    thread.join();
+
+    ioContext.run();
+
+    ASSERT_EQ(future.get(), "ReSuLt");
 }
