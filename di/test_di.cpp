b8cac539e6541035a7645f1ea0beb2e0d6dce5ce
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-06-21 16:45:59 +0300

    Test DI, factory with arg

--- di/test_di.cpp
+++ di/test_di.cpp
@@ -68,11 +68,49 @@ using FactoryBar = di::extension::factory<Bar>;
 
 } // Anonymouns namespace
 
-
 TEST(DI, factory_product) {
     auto injector = di::make_injector(
         di::bind<IFactoryBar>().to(FactoryBar{})
     );
-    auto& factory = injector.create<IFactoryBar&>();
-    auto bar = factory.create();
+    auto factory = injector.create<std::shared_ptr<IFactoryBar>>();
+    auto bar = factory->create();
+    ASSERT_EQ(bar->method(), 42);
+}
+
+
+namespace {
+
+
+struct IFoo
+{
+    virtual std::size_t method() const = 0;
+    virtual ~IFoo() = default;
+};
+
+struct Value
+{
+    std::size_t value;
+};
+
+struct Foo : IFoo
+{
+    Foo(const Value& p) : value{p.value} {}
+    virtual std::size_t method() const override { return value * 2; }
+
+    std::size_t value;
+};
+
+using IFactoryFoo = di::extension::ifactory<IFoo, Value>;
+using FactoryFoo = di::extension::factory<Foo>;
+
+
+} // Anonymous namespace
+
+TEST(DI, factory_product_with_arg) {
+    auto injector = di::make_injector(
+        di::bind<IFactoryFoo>().to(FactoryFoo{})
+    );
+    auto factory = injector.create<std::shared_ptr<IFactoryFoo>>();
+    auto foo = factory->create(Value{13});
+    ASSERT_EQ(foo->method(), 26);
 }
